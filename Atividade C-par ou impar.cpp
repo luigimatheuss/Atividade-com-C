7d3e30f11156073ea6a9e313e7bdc951d92df204
luigimatheuss <160722506+luigimatheuss@users.noreply.github.com>
2024-02-28 21:33:58 -0300

    Add files via upload

---
+++ Atividade C-par ou impar.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+	 int numero;
+
+    // Solicita ao usuário que insira um número inteiro
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+    // Verifica se o número é par ou ímpar
+    if (numero % 2 == 0) {
+       
+	    printf("%d eh um numero par.\n", numero);
+	    
+    } else {
+    	
+    	printf("%d eh um numero impar.\n", numero);
+    
+	}
+    
+	
+	
+	
+	
+	
+	
+}
