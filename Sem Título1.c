d6000394e2d1c3253bfdf78795a9541a5c04e9d8
luigimatheuss <160722506+luigimatheuss@users.noreply.github.com>
2024-03-13 08:41:28 -0300

    Add files via upload

--- Sem Título1.c
+++ Sem Título1.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
+#include <locale.h>
+
+void cabecalho() {
+	printf("===senai===\n");
+	system("cls");
+	fflush(stdin);
+}
 
 int main() {
-    int i;
+	setlocale(LC_ALL, "Portuguese");
+	float nota, soma = 0;
+	int contador = 0;
+	char resposta;
 
-    printf("Números ímpares entre 1 e 20:\n");
+	cabecalho();
 
+	do {
+		printf("Digite uma nota: ");
+		scanf("%f", &nota);
 
-    for(i = 1; i <= 20; i++) {
-        if(i % 2 != 0) {
-            printf("%d\n", i); // Imprime o número ímpar
-        }
-    }
+		soma += nota;
+		contador++;
 
-    return 0;
+		printf("Deseja inserir outra nota? (s/n): ");
+		scanf(" %c", &resposta);
+
+	} while (resposta == 'S' || resposta == 's');
+
+	if (contador > 0) {
+		float media = soma / contador;
+		printf("A média aritmética das notas inseridas é: %.2f\n", media);
+	} else {
+		printf("Nenhuma nota foi inserida.\n");
+	}
+
+	return 0;
 }
+
